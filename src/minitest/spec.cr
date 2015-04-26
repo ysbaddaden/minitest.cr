1605a3fd4d876d99e1aa00a9375e468fb4bb07a5
Julien Portalier <julien@portalier.com>
2015-04-26 16:25:59 +0200

    Better describe + added let for specs

--- src/minitest/spec.cr
+++ src/minitest/spec.cr
@@ -1,51 +1,86 @@
 module Minitest
   class Spec < Test
+    def after_teardown
+      super()
+
+      if memoized = @_memoized
+        memoized.clear
+      end
+    end
+
+    # NOTE: we can't use Reference as a generic (yet) so we can't just rely on a
+    #       Hash to memoize the let values and clear it on each teardown. We
+    #       thus keep a list of generated values to force the regeneration of a
+    #       variable on each test.
+    macro let(name, &block)
+      def {{ name.id }}
+        #@_memoized ||= {} of String => Reference
+        #@_memoized[{{ name.id.stringify }}] ||= begin; {{ block.body }}; end
+
+        memoized = @_memoized ||= [] of String
+
+        unless memoized.includes?({{ name.id.stringify }})
+          memoized << {{ name.id.stringify }}
+          @{{ name.id }} = nil
+        end
+
+        @{{ name.id }} ||= begin; {{ block.body }}; end
+      end
+    end
+
     macro before(&block)
       def setup
         super()
-        {{ block.body.id }}
+        {{ block.body }}
       end
     end
 
     macro after(&block)
       def teardown
-        {{ block.body.id }}
+        {{ block.body }}
         super()
       end
     end
 
     macro describe(name, &block)
       class {{
-              name.strip
-                .gsub(/[^0-9a-zA-Z]+/, "_")
-                .split("_")
-                .map { |s| s.capitalize }
-                .join("")
+              name.id.stringify.strip
+                .gsub(/[^0-9a-zA-Z:]+/, "_")
+                .split("_").map { |s| [s[0...1].upcase, s[1..-1]].join("") }.join("")
+                .split("::").map { |s| [s[0...1].upcase, s[1..-1]].join("") }.join("::")
                 .id
             }} < {{ @class_name.id }}
-        {{ block.body.id }}
+
+        def self.name
+          "#{ {{ @class_name.id }}.name }::{{ name.id }}"
+        end
+
+        {{ block.body }}
       end
     end
 
     macro it(name = "anonymous", &block)
-      def test_{{ name.strip.gsub(/[^0-9a-zA-Z]+/, "_").id }}
-        {{ block.body.id }}
+      def test_{{ name.strip.gsub(/[^0-9a-zA-Z:]+/, "_").id }}
+        {{ block.body }}
       end
     end
   end
 end
 
+# TODO: allow to inherit from specific classes (configurable with matcher)
 macro describe(name, &block)
   class {{
-          name.stringify
-            .strip
-            .gsub(/[^0-9a-zA-Z]+/, "_")
-            .split("_")
-            .map { |s| s.capitalize }
-            .join("")
+          name.id.stringify.strip
+            .gsub(/[^0-9a-zA-Z:]+/, "_")
+            .split("_").map { |s| s.capitalize }.join("")
+            .split("::").map { |s| s.capitalize }.join("::")
             .id
-        }}Spec < Minitest::Spec
-    {{ block.body.id }}
+        }}Test < Minitest::Spec
+
+    def self.name
+      {{ name.id.stringify }}
+    end
+
+    {{ block.body }}
   end
 end
-
--- test/spec_test.cr
+++ test/spec_test.cr
@@ -1,21 +1,49 @@
 require "../src/autorun"
 
 describe Minitest::Spec do
-  def value
-    "value"
+  class Foo
+    property :bar
   end
 
-  it "passes" do
+  def add(a, b)
+    a + b
+  end
+
+  let(:data) { "data value" }
+
+  it "accepts a block" do
     assert true
   end
 
-  it("accepts a bracket block") { assert true }
+  it("accepts a block with brackets") { assert true }
 
   it "calls an instance method" do
-    assert_equal "value", value
+    assert_equal 3, add(1, 2)
   end
 
-  it "musn't pass" do
-    assert_raises(Minitest::Assertion) { assert false }
+  describe("nested describes") do
+    let(:more) { "more " + data }
+
+    it "accesses parent methods" do
+      assert_equal 4, add(2, 2)
+    end
+
+    it "accesses let methods" do
+      assert_equal "data value", data
+      assert_equal "more data value", more
+    end
+  end
+
+  describe "let" do
+    let(:foo) { Foo.new }
+
+    it "memoizes the object for the duration of the test" do
+      foo.bar = "baz"
+      assert_equal "baz", foo.bar
+    end
+
+    it "regenerates the object on each teardown" do
+      assert_nil foo.bar
+    end
   end
 end
